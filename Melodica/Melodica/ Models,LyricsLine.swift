5a236a4f4223dd36ee2be14c5a883760cd5d819e
Yi0027 <zlobin.house27@gmail.com>
2026-08-19 02:06:08 +0300

    Reposting due to file copying error

--- Melodica/Melodica/ Models,LyricsLine.swift
+++
@@ -1,9 +0,0 @@
-// Models,LyricsLine.swift
-import Foundation
-
-struct LyricsLine: Identifiable {
-    let id = UUID()
-    let time: TimeInterval
-    let text: String
-    var pauseUntil: TimeInterval? 
-}
